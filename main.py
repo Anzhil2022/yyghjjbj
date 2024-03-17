8553e6ab4a0722227bf31f24a57e60e14455c76c
Anzhil2022 <159685358+Anzhil2022@users.noreply.github.com>
2024-03-17 11:31:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ player_move_animation = [pygame.image.load("player_walk_1.png"),
                          pygame.image.load("player_walk_3.png")]
 bullet_list = [] # список всех пулек на экране
 
-
+clock = pygame.time.Clock()
 current_sprite = 0
+player_on_platform = False
 def spawn_bullet(): # cоздоёт новою пулю на экране
     bullet_list.append([player_x, player_y, "right"]) # добавления пули в список
-
 def bullet_control(): # контролирует передвижения и попадание в цели
     for bullet in bullet_list:
         if bullet[2] == "right":
@@ -30,9 +30,9 @@ def move_char():
     global player_x
     global player_y
     if pressed_key == "d":
-        player_x += 5 # скорость передвижения персонажа
+        player_x += 1.5 # скорость передвижения персонажа
     if pressed_key == "a":
-        player_x -= 5  # скорость передвежения персонажа
+        player_x -= 1.5 # скорость передвежения персонажа
 
 def draw_scren (): # создания функций рисования экрана
     global current_sprite, player_x, player_y
@@ -44,25 +44,31 @@ def draw_scren (): # создания функций рисования экра
 def walk_anim():  # Функция, воспроизводящая анимацию спрайта
     global current_sprite
     if pressed_key != None:
-        current_sprite += 1
+        current_sprite += +1
         if len(player_move_animation) < current_sprite+1:
             current_sprite = 0
 
 
 def jump():
-    global  up_speed
-    up_speed = 10
+    global up_speed, player_y
+    up_speed = 20
+    player_y -= 5
 
 def physics_connrol():
     global player_y, up_speed
     player_y -= up_speed
     up_speed -= 1
+    if player_y+85 > screen_height-20:
+        up_speed = 0
+        player_y = screen_height-85
 
-pressed_key = None #
+pressed_key = None # отслеживает нажата ли клавиша движения
 i_count = 0 # счетчик для анимации
 up_speed = 0
 while True:
     i_count += 1
+    move_char()
+    physics_connrol()
     for event in pygame.event.get(): # Перебор всех событий (нажатия клавиш)
         if event.type == pygame.QUIT:
             exit()
@@ -81,12 +87,11 @@ while True:
         elif event.type == pygame.KEYUP:
             pressed_key = None
 
-    if i_count%60 == 0:
+    if i_count%8 == 0:
         walk_anim()
-    move_char()
     bullet_control()
-    physics_connrol()
     draw_scren()
+    clock.tick(60)
 
 
 
